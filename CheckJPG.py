e673a7727515b24bfc9f69624368fb48cfd178c1
nbazarian <nbazarian@gmail.com>
2022-07-13 20:01:16 -0400

    Removed Photos
    Removed more photos to hopefully resolve the dataset issues

--- CheckJPG.py
+++ CheckJPG.py
@@ -2,10 +2,10 @@ import glob
 import PIL
 from PIL import Image
 
-files = glob.glob('/Users/bazariann/Documents/MonkeyDeepLearnModel/NonHumanPrimateProject/test/*')
+files = glob.glob('/Users/bazariann/Documents/MonkeyDeepLearnModel/NonHumanPrimateProject/train/*')
 len(files)
 for file in files:
-    if '.jpg' in file:
+    if '.jpeg' in file:
         image = PIL.Image.open(file)
     if image.format not in ['JPG', 'JPEG']:
         print (file)
